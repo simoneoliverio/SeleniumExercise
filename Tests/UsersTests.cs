e3e79bd784d10af071d057cb241e2d13a886667b
simoneoliverio <oliverio.simone@gmail.com>
2018-03-04 15:20:24 +0100

    password

--- Tests/UsersTests.cs
+++ Tests/UsersTests.cs
@@ -10,7 +10,7 @@ namespace Tests
         [TestMethod]
         public void Can_Add_New_User()
         {
-            LoginPage.LoginAs("admin@mail.com").WithPassword("abc123").Login();
+            LoginPage.LoginAs("admin@mail.com").WithPassword("abc123aa").Login();
 
             UserPage.AddNewUser()
                 .WithFirstName("Maria")
